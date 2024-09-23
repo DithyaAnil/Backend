170fb0d9f82ab4818cf4fb52b4ee65b76e345acd
DithyaAnil <adithyaanil002@gmail.com>
2024-09-24 01:22:19 +0530

    GraphQL Nested Data

--- customers/schema.py
+++ customers/schema.py
@@ -1,10 +1,16 @@
 import graphene 
 from graphene_django import DjangoObjectType
-from customers.models import Customer
+from customers.models import Customer,Order
 
 class CustomerType(DjangoObjectType):
     class Meta:
         model = Customer
+        fields = '__all__'
+
+
+class OrderType(DjangoObjectType):
+    class Meta:
+        model = Order
         fields = '__all__'
 
 class CreateCustomer(graphene.Mutation):
@@ -21,6 +27,7 @@ class CreateCustomer(graphene.Mutation):
 
 class Query(graphene.ObjectType):
     customers = graphene.List(CustomerType)
+    orders=graphene.List(OrderType)
     customer_by_name = graphene.List(CustomerType,name = graphene.String(required=True))
 
     def resolve_customers(root, info):
@@ -31,6 +38,9 @@ class Query(graphene.ObjectType):
             return Customer.objects.filter(name=name)
         except Customer.DoesNotExist:
             return None
+        
+    def resolve_orders(root, info):
+        return Order.objects.select_related('customer').all()
     
 class Mutations(graphene.ObjectType):
     createCustomer = CreateCustomer.Field()
