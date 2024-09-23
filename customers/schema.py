247843cc660acc2c498f152e79b2dbf9a198822b
DithyaAnil <adithyaanil002@gmail.com>
2024-09-23 23:08:35 +0530

    Adding new query

--- customers/schema.py
+++ customers/schema.py
@@ -10,8 +10,15 @@ class CustomerType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     customers = graphene.List(CustomerType)
+    customer_by_name = graphene.List(CustomerType,name = graphene.String(required=True))
 
     def resolve_customers(root, info):
         return Customer.objects.all()
+    
+    def resolve_customer_by_name(root, info, name):
+        try:
+            return Customer.objects.filter(name=name)
+        except Customer.DoesNotExist:
+            return None
     
 schema = graphene.Schema(query=Query)
